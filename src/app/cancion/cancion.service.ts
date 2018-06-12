079e652f6b27fa76fe4d94a6b426616bccda3575
Jose Carlos Calzada <josecarlosplatos@gmail.com>
2018-06-12 10:45:48 +0200

    implementada busqueda

--- src/app/cancion/cancion.service.ts
+++ src/app/cancion/cancion.service.ts
@@ -2,24 +2,23 @@ import { Cancion } from './cancion.model';
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs/Observable";
+import { urlToNavGroupStrings } from 'ionic-angular/navigation/url-serializer';
 
 @Injectable()
 export class CancionService {
 
-    static url_itunes : string = "https://itunes.apple.com/search?term=dead+can+dance+host&media=music&limit=20"
 
-    constructor (private http : HttpClient)
+    constructor (public http : HttpClient)
     {
 
 
     }
 
-    getListaCancionessHttp (): Observable<Cancion[]> 
+    getListaCancionessHttp (url_recibida : string): Observable<Cancion[]> 
     {
         let lista_canciones : Observable<Cancion[]>;
 
-        lista_canciones = this.http.get<Cancion[]>
-        (CancionService.url_itunes);
+        lista_canciones = this.http.get<Cancion[]>(url_recibida);
 
         return lista_canciones;
     }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,27 +6,31 @@ import { NavController } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html',
-  styles: ['hr{border: 1px solid grey;}', 'ul{list-style:none;}', "audio{margin: -50px ;}"],
+  styles: ['hr{border: 1px solid grey;}', 'ul{list-style:none;}', "audio{margin-left: -25px;}"],
   providers: [CancionService]
 })
 export class HomePage {
 
-  private lista_canciones : Cancion[];
-  private busqueda : string;
+  public url_itunes : string;
+  public lista_canciones : Cancion[];
+  public busqueda : string;
 
-  constructor(cancion_service : CancionService , public navCtrl: NavController) {
+  constructor(private cancion_service : CancionService , public navCtrl: NavController) {
 
-    cancion_service.getListaCancionessHttp().subscribe
-    (ok => this.consumirListaCanciones(ok));
+   
 
   }
 
   consumirListaCanciones(ok : any){
+    console.log(ok);
     this.lista_canciones = <Cancion[]> ok.results;
   }
 
   buscar(){
     console.log(this.busqueda); 
+    this.url_itunes = "https://itunes.apple.com/search?term="+this.busqueda+"&media=music&limit=20"
+    this.cancion_service.getListaCancionessHttp(this.url_itunes).subscribe
+    (ok => this.consumirListaCanciones(ok));
   }
 
 }
